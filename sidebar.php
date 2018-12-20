7d1128f715412689e2ac8b1bb20eb3a1362f87ab
Dharmawan Sukma Hardi <dharmawansukmahardi@172-11-115-32.lightspeed.nworla.sbcglobal.net>
2018-12-20 14:49:25 +0700

    firs 2

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,10 @@
-<?php /* Template Name: Comming Soon */ ?>
+<?php
 
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
 
+<aside id="secondary" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
